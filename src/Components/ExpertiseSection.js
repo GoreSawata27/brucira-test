6caad44b7d33059a07107852a3e065802dccfb67
Sawata gore <sawatagore27@gmail.com>
2024-09-26 20:34:35 +0530

    fixed ui bugs

--- src/Components/ExpertiseSection.js
+++ src/Components/ExpertiseSection.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import accOne from "../assets/Expertise/Mask group.svg";
+import partnersOne from "../assets/unsplash__H6wpor9mjs.svg";
+import videoPreview from "../assets/HeroSection/image 793.svg";
 
 export default function ExpertiseSection() {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -15,9 +17,31 @@ export default function ExpertiseSection() {
           breathe life into your narratives
         </h2>
         <div className="grid grid-cols-[1fr_2fr] gap-8 md:grid-cols-[1fr_2fr] accordian-section">
-          <div className="flex accordian-img ">
-            <img src={accOne} alt="People clapping" className="rounded-lg " />
-          </div>
+          {(activeIndex === 0 || activeIndex === null) && (
+            <div className="flex accordian-img ">
+              <img src={accOne} alt="People clapping" className="rounded-lg " />
+            </div>
+          )}
+          {activeIndex === 1 && (
+            <div className="flex accordian-img ">
+              <img
+                src={partnersOne}
+                alt="People clapping"
+                className="rounded-lg "
+                style={{ maxWidth: "400px" }}
+              />
+            </div>
+          )}
+          {activeIndex === 2 && (
+            <div className="flex accordian-img ">
+              <img
+                src={videoPreview}
+                alt="People clapping"
+                className="rounded-lg "
+                style={{ maxWidth: "400px" }}
+              />
+            </div>
+          )}
 
           <div className="flex flex-col justify-start accordion-container ">
             <div className="flex flex-col justify-center">
@@ -28,8 +52,11 @@ export default function ExpertiseSection() {
                     className="flex items-center justify-between pb-4 border-b cursor-pointer"
                     onClick={() => toggleAccordion(0)}
                   >
-                    <span className="accordian-main-title">Innovative</span>
-                    <span>{activeIndex === 0 ? "-" : "+"}</span>
+                    <span>
+                      {activeIndex !== 0 && (
+                        <span className="accordian-main-title">Innovative</span>
+                      )}
+                    </span>
                   </div>
                   <div
                     className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
@@ -55,11 +82,16 @@ export default function ExpertiseSection() {
                 {/* 2 */}
                 <div>
                   <div
-                    className="flex items-center justify-between pb-4 border-b cursor-pointer"
+                    className={`flex items-center justify-between pb-4  cursor-pointer ${
+                      activeIndex !== 1 && "border-b"
+                    }`}
                     onClick={() => toggleAccordion(1)}
                   >
-                    <span className="accordian-main-title">Reliable</span>
-                    <span>{activeIndex === 1 ? "-" : "+"}</span>
+                    <span>
+                      {activeIndex !== 1 && (
+                        <span className="accordian-main-title">Reliable</span>
+                      )}
+                    </span>
                   </div>
                   <div
                     className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
@@ -85,11 +117,16 @@ export default function ExpertiseSection() {
                 {/* 3 */}
                 <div>
                   <div
-                    className="flex items-center justify-between pb-4 border-b cursor-pointer"
+                    className={`flex items-center justify-between pb-4  cursor-pointer ${
+                      activeIndex !== 2 && "border-b"
+                    }`}
                     onClick={() => toggleAccordion(2)}
                   >
-                    <span className="accordian-main-title">Quality</span>
-                    <span>{activeIndex === 2 ? "-" : "+"}</span>
+                    <span>
+                      {activeIndex !== 2 && (
+                        <span className="accordian-main-title">Quality</span>
+                      )}
+                    </span>
                   </div>
                   <div
                     className={`transition-max-height duration-500 ease-in-out overflow-hidden ${
